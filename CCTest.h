248352ded658e3a1384e1c09aa9b65daaf3a9b5e
Vijay Ayyar <vayyar@vayyar-macbookair.roam.corp.google.com>
2014-08-13 18:24:23 +0800

    added a test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Vijay Ayyar on 8/13/14.
+//  Copyright (c) 2014 Vijay Ayyar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Vijay Ayyar on 8/13/14.
+//  Copyright (c) 2014 Vijay Ayyar. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
